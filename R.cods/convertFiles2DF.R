f6a08f224aad9d26f523fcfcde05bb2e08761ebd
mirjalil <vmirjalily@gmail.com>
2015-04-21 01:51:06 -0400

    R.code for combining Ensemble Results

--- R.cods/convertFiles2DF.R
+++ R.cods/convertFiles2DF.R
@@ -1,21 +1,30 @@
-gl <- list.files(path="results/hf1/", pattern="pred*", full.names=T)
+args <- args <- commandArgs(trailingOnly = TRUE)
 
+gl <- list.files(path=paste("results/", args[1], sep=""), pattern="pred*", full.names=T)
+print(gl[1])
 
 cvpaste <- function(str) {
 	paste(str, ".cv", sep="")
 }
 
 fl <- sapply(gsub("//", "/cv/", gl), cvpaste)
+print(fl[1])
 
 dl <- lapply(fl, read.table)
+print(str(dl[[1]]))
 
 df <- do.call("cbind", dl)
+print(str(df[,1:5]))
 
-write.table(df, file="results/hf1/res_cv.dat", row.names=F, col.names=F)
+
+write.table(df, file=paste("results/", args[1], "/res_cv.dat", sep=""), row.names=F, col.names=F)
 
 
 ### Readine the predictions for the test dataset
 dl <- lapply(gl, read.table)
-df <- do.call("cbind", dl)
+print(str(dl[[1]]))
 
-write.table(df, file="results/hf1/res_test.dat", row.names=F, col.names=F)
+df <- do.call("cbind", dl)
+print (str(df[,1:5]))
+
+write.table(df, file=paste("results/", args[1], "/res_test.dat", sep=""), row.names=F, col.names=F)
